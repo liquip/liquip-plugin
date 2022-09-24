60e6b76b9c8706c882d1a4c37a2cefa6f5ed583b
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-09-24 17:29:14 +0200

    Add registry to Liquip.java

--- src/main/java/com/github/sqyyy/liquip/Liquip.java
+++ src/main/java/com/github/sqyyy/liquip/Liquip.java
@@ -1,7 +1,19 @@
 package com.github.sqyyy.liquip;
 
+import com.github.sqyyy.liquip.items.Feature;
+import com.github.sqyyy.liquip.util.Registry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Liquip extends JavaPlugin {
+    public static final String DEFAULT_NAMESPACE = "minecraft";
+    private final Registry<Feature> features = new Registry<>();
 
+    @Override
+    public void onEnable() {
+
+    }
+
+    public Registry<Feature> getFeatures() {
+        return features;
+    }
 }
