648bdda8f1bae40e02327c9a789c524efde69be4
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-09-29 19:01:32 +0200

    Make Registry more abstract

--- src/main/java/com/github/sqyyy/liquip/util/Registry.java
+++ src/main/java/com/github/sqyyy/liquip/util/Registry.java
@@ -3,42 +3,64 @@ package com.github.sqyyy.liquip.util;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Registry<T> {
-    private final Map<Identifier, T> registry;
-    private boolean locked;
+public interface Registry<T> {
+    boolean register(Identifier identifier, T value);
 
-    public Registry() {
-        registry = new HashMap<>();
-        locked = false;
-    }
+    boolean unregister(Identifier identifier);
 
-    public boolean register(Identifier identifier, T value) {
-        if (locked) {
-            return false;
+    boolean isRegistered(Identifier identifier);
+
+    void lock();
+
+    boolean isLocked();
+
+    T get(Identifier identifier);
+
+    class BasicRegistry<T> implements Registry<T> {
+        private final Map<Identifier, T> registry;
+        private boolean locked;
+
+        public BasicRegistry() {
+            registry = new HashMap<>();
+            locked = false;
         }
-        if (registry.containsKey(identifier)) {
-            return false;
-        }
-        registry.put(identifier, value);
-        return true;
-    }
 
-    public boolean unregister(Identifier identifier) {
-        if (locked) {
-            return false;
+        public boolean register(Identifier identifier, T value) {
+            if (locked) {
+                return false;
+            }
+            if (registry.containsKey(identifier)) {
+                return false;
+            }
+            registry.put(identifier, value);
+            return true;
         }
-        if (!registry.containsKey(identifier)) {
-            return false;
+
+        public boolean unregister(Identifier identifier) {
+            if (locked) {
+                return false;
+            }
+            if (!registry.containsKey(identifier)) {
+                return false;
+            }
+            registry.remove(identifier);
+            return true;
         }
-        registry.remove(identifier);
-        return true;
-    }
 
-    public void lock() {
-        locked = true;
-    }
+        public boolean isRegistered(Identifier identifier) {
+            return registry.containsKey(identifier);
+        }
 
-    public T get(Identifier identifier) {
-        return registry.get(identifier);
+        public void lock() {
+            locked = true;
+        }
+
+        public boolean isLocked() {
+            return locked;
+        }
+
+        public T get(Identifier identifier) {
+            return registry.get(identifier);
+        }
     }
 }
