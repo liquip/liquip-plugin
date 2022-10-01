6af12875e701712a167946e8c9f16ffadac5f5a3
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-01 12:43:37 +0200

    Add Enchantment-parsing to LeveledEnchantment

--- src/main/java/com/github/sqyyy/liquip/items/LeveledEnchantment.java
+++ src/main/java/com/github/sqyyy/liquip/items/LeveledEnchantment.java
@@ -1,8 +1,11 @@
 package com.github.sqyyy.liquip.items;
 
 import com.github.sqyyy.liquip.util.Identifier;
+import com.github.sqyyy.liquip.util.Registry;
+import com.github.sqyyy.liquip.util.Result;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Optional;
 
@@ -10,13 +13,31 @@ public class LeveledEnchantment {
     private final Object enchantment;
     private final int level;
 
-    public LeveledEnchantment(Identifier key, int level) {
-        if (key.getKey().equals("minecraft")) {
-            enchantment = Enchantment.getByKey(new NamespacedKey(key.getNamespace(), key.getKey()));
-        } else {
-            throw new RuntimeException("Not implemented");
-        }
+    public LeveledEnchantment(Enchantment enchantment, int level) {
+        this.enchantment = enchantment;
         this.level = level;
+    }
+
+    public LeveledEnchantment(LiquipEnchantment enchantment, int level) {
+        this.enchantment = enchantment;
+        this.level = level;
+    }
+
+    public static Result<LeveledEnchantment, LiquipError> parse(Identifier key, int level,
+                                                                Registry<LiquipEnchantment> enchantmentRegistry) {
+
+        if (key.getNamespace().equals("minecraft")) {
+            return Result.ok(new LeveledEnchantment(Enchantment.getByKey(new NamespacedKey(key.getNamespace(),
+                    key.getKey())), level));
+        } else {
+            if (!enchantmentRegistry.isRegistered(key)) {
+                return Result.err(LiquipError.ENCHANTMENT_NOT_FOUND);
+            }
+
+            final var enchantmentResult = enchantmentRegistry.get(key);
+
+            return Result.ok(new LeveledEnchantment(enchantmentResult, level));
+        }
     }
 
     public Object getEnchantment() {
@@ -41,5 +62,15 @@ public class LeveledEnchantment {
 
     public int getLevel() {
         return level;
+    }
+
+    public void apply(ItemStack itemStack) {
+        if (enchantment instanceof LiquipEnchantment liquipEnchantment) {
+            liquipEnchantment.apply(itemStack, level);
+            return;
+        }
+        if (enchantment instanceof Enchantment bukkitEnchantment) {
+            itemStack.addUnsafeEnchantment(bukkitEnchantment, level);
+        }
     }
 }
