c80706a9c0a8179b5e3a7c89343f9438700f32f7
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-01 12:41:44 +0200

    Add ConfigErrors

--- src/main/java/com/github/sqyyy/liquip/config/ConfigError.java
+++ src/main/java/com/github/sqyyy/liquip/config/ConfigError.java
@@ -3,12 +3,14 @@ package com.github.sqyyy.liquip.config;
 public enum ConfigError {
     CONFIG_IO_EXCEPTION("An IO-Exception was thrown"),
     CONFIG_IS_DIRECTORY("The config is not a file"),
+    COULD_NOT_REGISTER("The item could not be registered"),
     PLUGIN_DIRECTORY_DOES_NOT_EXIST("The plugin-directory does not exist"),
     NULL_CONFIG("A part of the config is null"),
     NO_ITEMS_REGISTRY("No items-registry was found"),
     ITEM_FILE_NOT_FOUND("The item-file was not found"),
     ITEM_FILE_IS_DIRECTORY("The item-file is not a file"),
     ITEM_INVALID("The item is invalid"),
+    ITEM_PATH_INVALID("The path to the item-file is invalid"),
     ITEMS_DIRECTORY_IS_FILE("The items-directory is not a directory"),
     ITEMS_REGISTRY_WRONG_TYPE("The items-registry is of wrong type, should be list");
 
