d32b4d45a507c071afbed49eb6bdc4648a1c3afa
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-01 12:41:14 +0200

    Add Enchantment-registry & DevCommand

--- src/main/java/com/github/sqyyy/liquip/Liquip.java
+++ src/main/java/com/github/sqyyy/liquip/Liquip.java
@@ -1,8 +1,11 @@
 package com.github.sqyyy.liquip;
 
 import com.github.sqyyy.liquip.config.ConfigLoader;
+import com.github.sqyyy.liquip.dev.DevCommand;
 import com.github.sqyyy.liquip.items.Feature;
+import com.github.sqyyy.liquip.items.LiquipEnchantment;
 import com.github.sqyyy.liquip.items.LiquipItem;
+import com.github.sqyyy.liquip.util.BasicRegistry;
 import com.github.sqyyy.liquip.util.Registry;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,8 +14,9 @@ import org.slf4j.Logger;
 
 public class Liquip extends JavaPlugin {
     public static final String DEFAULT_NAMESPACE = "minecraft";
-    private final Registry<Feature> features = new Registry.BasicRegistry<>();
-    private final Registry<LiquipItem> items = new Registry.BasicRegistry<>();
+    private final Registry<Feature> features = new BasicRegistry<>();
+    private final Registry<LiquipItem> items = new BasicRegistry<>();
+    private final Registry<LiquipEnchantment> enchantments = new BasicRegistry<>();
     private final Logger logger = getSLF4JLogger();
 
     @Override
@@ -24,14 +28,26 @@ public class Liquip extends JavaPlugin {
             logger.error("An error occurred while loading config: {}", configResult.get().getMessage());
             Bukkit.getPluginManager().disablePlugin(this);
         }
+
+        final var devCommand = getCommand("liquip");
+
+        if (devCommand == null) {
+            return;
+        }
+
+        devCommand.setExecutor(new DevCommand(this));
     }
 
-    public Registry<Feature> getFeatures() {
+    public Registry<Feature> getFeatureRegistry() {
         return features;
     }
 
-    public Registry<LiquipItem> getItems() {
+    public Registry<LiquipItem> getItemRegistry() {
         return items;
+    }
+
+    public Registry<LiquipEnchantment> getEnchantmentRegistry() {
+        return enchantments;
     }
 
     @NotNull
