e8924a7cab3013c1066665a1bde6644e3c1af694
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-01 12:41:58 +0200

    Update Feature

--- src/main/java/com/github/sqyyy/liquip/items/Feature.java
+++ src/main/java/com/github/sqyyy/liquip/items/Feature.java
@@ -3,5 +3,8 @@ package com.github.sqyyy.liquip.items;
 import org.bukkit.inventory.ItemStack;
 
 public interface Feature {
-    void apply(ItemStack item);
+    // TODO set feature-format
+    void initialize(LiquipItem item);
+
+    void apply(ItemStack itemStack);
 }
