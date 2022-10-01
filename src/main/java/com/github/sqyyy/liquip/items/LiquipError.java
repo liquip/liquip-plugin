95659c14669fe7023741fcaa67e499aa928f17d4
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-01 12:42:08 +0200

    Update LiquipError

--- src/main/java/com/github/sqyyy/liquip/items/LiquipError.java
+++ src/main/java/com/github/sqyyy/liquip/items/LiquipError.java
@@ -1,12 +1,30 @@
 package com.github.sqyyy.liquip.items;
 
 public enum LiquipError {
-    NO_KEY_FOUND,
-    NO_NAME_FOUND,
-    NO_MATERIAL_FOUND,
-    INVALID_KEY,
-    INVALID_NAME,
-    INVALID_MATERIAL,
-    INVALID_ENCHANTMENT,
-    WRONG_TYPE,
+    NO_KEY_FOUND("No key was found"),
+    NO_NAME_FOUND("No name was found"),
+    NO_MATERIAL_FOUND("No material was found"),
+    INVALID_KEY("The key is invalid"),
+    INVALID_NAME("The name is invalid"),
+    INVALID_MATERIAL("The material is invalid"),
+    INVALID_ENCHANTMENT("An enchantment is invalid"),
+    INVALID_FEATURE("A feature is invalid"),
+    ENCHANTMENT_NOT_FOUND("An enchantment was not found"),
+    FEATURE_NOT_FOUND("A feature was not found"),
+    WRONG_TYPE("A value has the wrong type");
+
+    private final String message;
+
+    LiquipError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + super.toString() + "}" + message;
+    }
 }
