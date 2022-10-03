48624ba91412ce4498749738ed42574ae6eba78b
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-03 12:02:20 +0200

    Add MinecraftFeatures

---
+++ src/main/java/com/github/sqyyy/liquip/features/MinecraftFeatures.java
@@ -0,0 +1,33 @@
+package com.github.sqyyy.liquip.features;
+
+import com.github.sqyyy.liquip.items.Feature;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+
+public class MinecraftFeatures {
+    private MinecraftFeatures() {
+    }
+
+    public static class Unbreakable implements Feature {
+        @Override
+        public void apply(ItemStack itemStack) {
+            final var itemMeta = itemStack.getItemMeta();
+            itemMeta.setUnbreakable(true);
+            itemStack.setItemMeta(itemMeta);
+        }
+    }
+
+    public static class HideEnchantments implements Feature {
+        @Override
+        public void apply(ItemStack itemStack) {
+            itemStack.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+    }
+
+    public static class HideUnbreakable implements Feature {
+        @Override
+        public void apply(ItemStack itemStack) {
+            itemStack.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        }
+    }
+}
