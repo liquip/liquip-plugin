8506ab3ab8da2d0e01efac9493cb27ccf4e188f0
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-03 12:04:51 +0200

    Add method for feature-loading to LiquipProvider

--- src/main/java/com/github/sqyyy/liquip/LiquipProvider.java
+++ src/main/java/com/github/sqyyy/liquip/LiquipProvider.java
@@ -1,12 +1,16 @@
 package com.github.sqyyy.liquip;
 
+import com.github.sqyyy.liquip.features.MinecraftFeatures;
+import com.github.sqyyy.liquip.features.custom.GrapplingHook;
 import com.github.sqyyy.liquip.items.Feature;
 import com.github.sqyyy.liquip.items.LiquipEnchantment;
 import com.github.sqyyy.liquip.items.LiquipItem;
-import com.github.sqyyy.liquip.util.BasicRegistry;
+import com.github.sqyyy.liquip.util.impl.BasicRegistry;
+import com.github.sqyyy.liquip.util.Identifier;
 import com.github.sqyyy.liquip.util.Registry;
 
 public class LiquipProvider {
+    public static final String DEFAULT_NAMESPACE = "minecraft";
     private final Registry<Feature> features;
     private final Registry<LiquipItem> items;
     private final Registry<LiquipEnchantment> enchantments;
@@ -27,5 +31,19 @@ public class LiquipProvider {
 
     public Registry<LiquipEnchantment> getEnchantmentRegistry() {
         return enchantments;
+    }
+
+    public void registerDefaultFeatures(boolean customFeatures) {
+        features.register(new Identifier(DEFAULT_NAMESPACE, "unbreakable"), new MinecraftFeatures.Unbreakable());
+        features.register(new Identifier(DEFAULT_NAMESPACE, "hide_enchantments"),
+                new MinecraftFeatures.HideEnchantments());
+        features.register(new Identifier(DEFAULT_NAMESPACE, "hide_unbreakable"),
+                new MinecraftFeatures.HideUnbreakable());
+
+        if (!customFeatures) {
+            return;
+        }
+
+        features.register(new Identifier("liquip", "grappling_hook"), new GrapplingHook());
     }
 }
