7428a9ec166f7381605a2e9d2f2a06f5cb285e56
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-08 18:13:43 +0200

    Add experimental craft subcommand to DevCommand

--- core/src/main/java/com/github/sqyyy/liquip/core/dev/DevCommand.java
+++ core/src/main/java/com/github/sqyyy/liquip/core/dev/DevCommand.java
@@ -1,32 +1,54 @@
 package com.github.sqyyy.liquip.core.dev;
 
-import com.github.sqyyy.liquip.core.util.Identifier;
 import com.github.sqyyy.liquip.core.Liquip;
+import com.github.sqyyy.liquip.core.util.Identifier;
+import com.github.sqyyy.liquip.gui.Menu;
+import com.github.sqyyy.liquip.gui.MenuType;
+import com.github.sqyyy.liquip.gui.Slot;
+import com.github.sqyyy.liquip.gui.impl.BasicMenu;
+import com.github.sqyyy.liquip.gui.impl.FillPane;
+import com.github.sqyyy.liquip.gui.impl.OutputSlotPane;
+import com.github.sqyyy.liquip.gui.impl.StoragePane;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class DevCommand implements CommandExecutor {
     private final Liquip liquip;
+    private final Menu menu;
 
     public DevCommand(Liquip liquip) {
         this.liquip = liquip;
+        Menu.initialize(liquip);
+        menu = new BasicMenu(Component.text("Advanced Crafting"), 5, MenuType.CHEST, new List[]{
+                List.of(new FillPane(0, Slot.ROW_ONE_SLOT_ONE, Slot.ROW_FIVE_SLOT_NINE,
+                        new ItemStack(Material.BLACK_STAINED_GLASS_PANE))),
+                List.of(new FillPane(1, Slot.ROW_TWO_SLOT_SIX, Slot.ROW_FOUR_SLOT_EIGHT,
+                        new ItemStack(Material.LIME_STAINED_GLASS_PANE))),
+                List.of(new StoragePane(2, Slot.ROW_TWO_SLOT_TWO, Slot.ROW_FOUR_SLOT_FOUR, (storagePane, inventory) -> {
+                }, (storagePane, inventoryCloseEvent) -> {
+                }), new OutputSlotPane(2, Slot.ROW_THREE_SLOT_SEVEN))});
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
                              @NotNull String[] args) {
         MiniMessage mm = MiniMessage.miniMessage();
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player player)) {
             sender.sendMessage(mm.deserialize("<red>You need to be a player to use that command</red>"));
             return true;
         }
 
         if (args.length == 0) {
-            sender.sendMessage(mm.deserialize("<red>Usage: /liquip give <item></red>"));
+            player.sendMessage(mm.deserialize("<red>Usage: /liquip give <item></red>"));
             return true;
         }
 
@@ -35,7 +57,7 @@ public class DevCommand implements CommandExecutor {
         switch (subcommand.toLowerCase()) {
             case "give" -> {
                 if (args.length != 2) {
-                    sender.sendMessage(mm.deserialize("<red>Too many arguments</red>"));
+                    player.sendMessage(mm.deserialize("<red>Too many arguments</red>"));
                     return true;
                 }
 
@@ -43,7 +65,7 @@ public class DevCommand implements CommandExecutor {
                 final var identifierResult = Identifier.parse(identifierArg, Liquip.DEFAULT_NAMESPACE);
 
                 if (identifierResult.isErr()) {
-                    sender.sendMessage(mm.deserialize("<red>Invalid identifier</red>"));
+                    player.sendMessage(mm.deserialize("<red>Invalid identifier</red>"));
                     return true;
                 }
 
@@ -51,14 +73,17 @@ public class DevCommand implements CommandExecutor {
                 final var item = Liquip.getProvider().getItemRegistry().get(identifier);
 
                 if (item == null) {
-                    sender.sendMessage(mm.deserialize("<red>The supplied item could not be found</red>"));
+                    player.sendMessage(mm.deserialize("<red>The supplied item could not be found</red>"));
                     return true;
                 }
 
-                ((Player) sender).getInventory().addItem(item.newItem());
+                player.getInventory().addItem(item.newItem());
+            }
+            case "craft" -> {
+                menu.open(player);
             }
             default -> {
-                sender.sendMessage(mm.deserialize("<red>Unknown subcommand</red>"));
+                player.sendMessage(mm.deserialize("<red>Unknown subcommand</red>"));
                 return true;
             }
         }
