ce0ab83b3e709c2b76bec201a19c57a21e656d10
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-12 12:22:46 +0200

    Add AspectOfTheEnd feature for example-plugin

--- example-plugin/src/main/java/com/github/sqyyy/liquip/example/LiquipExample.java
+++ example-plugin/src/main/java/com/github/sqyyy/liquip/example/LiquipExample.java
@@ -3,15 +3,17 @@ package com.github.sqyyy.liquip.example;
 import com.github.sqyyy.liquip.core.Liquip;
 import com.github.sqyyy.liquip.core.LiquipProvider;
 import com.github.sqyyy.liquip.core.util.Identifier;
+import com.github.sqyyy.liquip.example.features.AspectOfTheEnd;
 import com.github.sqyyy.liquip.example.features.GrapplingHook;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LiquipExample extends JavaPlugin {
-    private final String namespace = "liquip-example";
+    private final String namespace = "liquip";
 
     @Override
     public void onLoad() {
         final LiquipProvider provider = Liquip.getProvider();
         provider.getFeatureRegistry().register(new Identifier(namespace, "grappling_hook"), new GrapplingHook());
+        provider.getFeatureRegistry().register(new Identifier(namespace, "aspect_of_the_end"), new AspectOfTheEnd());
     }
 }
