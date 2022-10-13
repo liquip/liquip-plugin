76251a2b834e0524e547480725343cca81f6c3f3
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-13 11:40:52 +0200

    Add the ability to create several attribute modifiers

--- core/src/main/java/com/github/sqyyy/liquip/core/items/impl/AttributeModifierModifierSupplier.java
+++ core/src/main/java/com/github/sqyyy/liquip/core/items/impl/AttributeModifierModifierSupplier.java
@@ -10,57 +10,66 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class AttributeModifierModifierSupplier implements ModifierSupplier {
     @Override
     public @Nullable Modifier get(@NotNull Config config) {
-        if (!config.hasPath("attribute") || !config.hasPath("name") || !config.hasPath("amount") ||
-                !config.hasPath("operation")) {
+        if (!config.hasPath("entries")) {
             return null;
         }
         try {
-            final String attributeString = config.getString("attribute");
-            Attribute attribute = null;
-            for (Attribute value : Attribute.values()) {
-                if (value.getKey().equals(NamespacedKey.minecraft(attributeString))) {
-                    attribute = value;
-                    break;
+            final List<? extends Config> attributeModifiers = config.getConfigList("entries");
+            final List<AttributeModifiersModifier.Entry> entries = new ArrayList<>();
+            for (Config attributeModifier : attributeModifiers) {
+                if (!attributeModifier.hasPath("attribute") || !attributeModifier.hasPath("name") ||
+                        !attributeModifier.hasPath("amount") || !attributeModifier.hasPath("operation")) {
+                    continue;
                 }
-            }
-            if (attributeString == null) {
-                return null;
-            }
-            final String name = config.getString("name");
-            double amount = config.getDouble("amount");
-            final String operationString = config.getString("operation").toUpperCase();
-            AttributeModifier.Operation operation = null;
-            switch (operationString.toLowerCase()) {
-                case "add" -> operation = AttributeModifier.Operation.ADD_NUMBER;
-                case "subtract" -> {
-                    amount = -amount;
-                    operation = AttributeModifier.Operation.ADD_NUMBER;
-                }
-                case "multiply" -> {
-                    amount -= 1;
-                    operation = AttributeModifier.Operation.MULTIPLY_SCALAR_1;
-                }
-            }
-            if (operation == null) {
-                return null;
-            }
-            EquipmentSlot slot = EquipmentSlot.HAND;
-            if (config.hasPath("slot")) {
-                final String slotString = config.getString("slot").toUpperCase();
-                for (EquipmentSlot value : EquipmentSlot.values()) {
-                    if (value.toString().equals(slotString)) {
-                        slot = value;
+                final String attributeString = attributeModifier.getString("attribute");
+                Attribute attribute = null;
+                for (Attribute value : Attribute.values()) {
+                    if (value.getKey().equals(NamespacedKey.minecraft(attributeString))) {
+                        attribute = value;
                         break;
                     }
                 }
+                if (attribute == null) {
+                    continue;
+                }
+                final String name = attributeModifier.getString("name");
+                double amount = attributeModifier.getDouble("amount");
+                final String operationString = attributeModifier.getString("operation").toUpperCase();
+                AttributeModifier.Operation operation = null;
+                switch (operationString.toLowerCase()) {
+                    case "add" -> operation = AttributeModifier.Operation.ADD_NUMBER;
+                    case "subtract" -> {
+                        amount = -amount;
+                        operation = AttributeModifier.Operation.ADD_NUMBER;
+                    }
+                    case "multiply" -> {
+                        operation = AttributeModifier.Operation.MULTIPLY_SCALAR_1;
+                    }
+                }
+                if (operation == null) {
+                    continue;
+                }
+                EquipmentSlot slot = EquipmentSlot.HAND;
+                if (attributeModifier.hasPath("slot")) {
+                    final String slotString = attributeModifier.getString("slot").toUpperCase();
+                    for (EquipmentSlot value : EquipmentSlot.values()) {
+                        if (value.toString().equals(slotString)) {
+                            slot = value;
+                            break;
+                        }
+                    }
+                }
+                entries.add(new AttributeModifiersModifier.Entry(attribute,
+                        new AttributeModifier(UUID.randomUUID(), name, amount, operation, slot)));
             }
-            return new AttributeModifierModifier(attribute,
-                    new AttributeModifier(UUID.randomUUID(), name, amount, operation, slot));
+            return new AttributeModifiersModifier(entries);
         } catch (Exception ignored) {
             return null;
         }
