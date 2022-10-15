ee2b082939dfd34d92cecccc55023dbd9e4600f9
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-15 18:42:45 +0200

    Add PlayerBucketEmptyEvent to event-listener

--- core/src/main/java/com/github/sqyyy/liquip/core/event/PlayerEventListener.java
+++ core/src/main/java/com/github/sqyyy/liquip/core/event/PlayerEventListener.java
@@ -6,10 +6,7 @@ import com.github.sqyyy.liquip.core.util.Identifier;
 import net.minecraft.world.item.ItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
@@ -24,15 +21,13 @@ public class PlayerEventListener implements Listener {
         final String identifierString = handle.tag.getString("liquip:identifier");
         final Optional<Identifier> parsedIdentifier = Identifier.parse(identifierString);
         if (parsedIdentifier.isEmpty()) {
-            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
-                    .error("{PlayerInteractEvent} Item with invalid identifier");
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger().error("{PlayerInteractEvent} Item with invalid identifier");
             return;
         }
         final Identifier identifier = parsedIdentifier.get();
         final LiquipItem liquipItem = Liquip.getProvider().getItemRegistry().get(identifier);
         if (liquipItem == null) {
-            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
-                    .error("{PlayerInteractEvent} Item with unknown identifier");
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger().error("{PlayerInteractEvent} Item with unknown identifier");
             return;
         }
         liquipItem.callEvent(PlayerInteractEvent.class, event);
@@ -92,17 +87,37 @@ public class PlayerEventListener implements Listener {
         final String identifierString = handle.tag.getString("liquip:identifier");
         final Optional<Identifier> parsedIdentifier = Identifier.parse(identifierString);
         if (parsedIdentifier.isEmpty()) {
-            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
-                    .error("{PlayerFishEvent} Item with invalid identifier");
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger().error("{PlayerFishEvent} Item with invalid identifier");
             return;
         }
         final Identifier identifier = parsedIdentifier.get();
         final LiquipItem liquipItem = Liquip.getProvider().getItemRegistry().get(identifier);
         if (liquipItem == null) {
-            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
-                    .error("{PlayerFishEvent} Item with unknown identifier");
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger().error("{PlayerFishEvent} Item with unknown identifier");
             return;
         }
         liquipItem.callEvent(PlayerFishEvent.class, event);
+    }
+
+    @EventHandler
+    public void onBucketEmpty(@NotNull PlayerBucketEmptyEvent event) {
+        final ItemStack handle = Util.checkPlayer(event.getPlayer());
+        if (handle == null) {
+            return;
+        }
+        final String identifierString = handle.tag.getString("liquip:identifier");
+        final Optional<Identifier> identifier = Identifier.parse(identifierString);
+        if (identifier.isEmpty()) {
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
+                    .error("{PlayerBucketEmptyEvent} Item with invalid identifier");
+            return;
+        }
+        final LiquipItem liquipItem = Liquip.getProvider().getItemRegistry().get(identifier.get());
+        if (liquipItem == null) {
+            Liquip.getProvidingPlugin(Liquip.class).getSLF4JLogger()
+                    .error("{PlayerBucketEmptyEvent} Item with unknown identifier");
+            return;
+        }
+        liquipItem.callEvent(PlayerBucketEmptyEvent.class, event);
     }
 }
