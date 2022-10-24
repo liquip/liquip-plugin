977f3353bba2013caae03b9cd85d56e2fcc32030
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-24 16:25:25 +0200

    [dev] Add example-plugin checked registering

--- example-plugin/src/main/java/com/github/sqyyy/liquip/example/LiquipExample.java
+++ example-plugin/src/main/java/com/github/sqyyy/liquip/example/LiquipExample.java
@@ -2,12 +2,20 @@ package com.github.sqyyy.liquip.example;
 
 import com.github.sqyyy.liquip.core.Liquip;
 import com.github.sqyyy.liquip.core.LiquipProvider;
+import com.github.sqyyy.liquip.core.items.Feature;
 import com.github.sqyyy.liquip.core.util.Identifier;
+import com.github.sqyyy.liquip.core.util.Registry;
 import com.github.sqyyy.liquip.example.features.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class LiquipExample extends JavaPlugin {
     private final String NAMESPACE = "liquip";
+    private Map<UUID, PlayerData> playerData = new HashMap<>();
     private GrapplingHook grapplingHook;
     private AspectOfTheEnd aspectOfTheEnd;
     private MagicalWaterBucket magicalWaterBucket;
@@ -17,20 +25,32 @@ public class LiquipExample extends JavaPlugin {
     @Override
     public void onLoad() {
         grapplingHook = new GrapplingHook();
-        aspectOfTheEnd = new AspectOfTheEnd();
+        aspectOfTheEnd = new AspectOfTheEnd(this);
         magicalWaterBucket = new MagicalWaterBucket();
         treecapitator = new Treecapitator(new Treecapitator.Queue(1024, 128));
-        staffOfPower = new StaffOfPower();
+        staffOfPower = new StaffOfPower(this);
         final LiquipProvider provider = Liquip.getProvider();
-        provider.getFeatureRegistry().register(new Identifier(NAMESPACE, "grappling_hook"), grapplingHook);
-        provider.getFeatureRegistry().register(new Identifier(NAMESPACE, "aspect_of_the_end"), aspectOfTheEnd);
-        provider.getFeatureRegistry().register(new Identifier(NAMESPACE, "magical_water_bucket"), magicalWaterBucket);
-        provider.getFeatureRegistry().register(new Identifier(NAMESPACE, "treecapitator"), treecapitator);
-        provider.getFeatureRegistry().register(new Identifier(NAMESPACE, "staff_of_power"), staffOfPower);
+        final Registry<Feature> featureRegistry = provider.getFeatureRegistry();
+        registerChecked(featureRegistry, new Identifier(NAMESPACE, "grappling_hook"), grapplingHook);
+        registerChecked(featureRegistry, new Identifier(NAMESPACE, "aspect_of_the_end"), aspectOfTheEnd);
+        registerChecked(featureRegistry, new Identifier(NAMESPACE, "magical_water_bucket"), magicalWaterBucket);
+        registerChecked(featureRegistry, new Identifier(NAMESPACE, "treecapitator"), treecapitator);
+        registerChecked(featureRegistry, new Identifier(NAMESPACE, "staff_of_power"), staffOfPower);
+    }
+
+    private <T> void registerChecked(Registry<T> featureRegistry, Identifier identifier, T entry) {
+        if (!featureRegistry.register(identifier, entry)) {
+            throw new IllegalStateException(identifier + " already registered");
+        }
     }
 
     @Override
     public void onEnable() {
         treecapitator.start();
+        Bukkit.getPluginManager().registerEvents(new ExampleEventHandler(this), this);
+    }
+
+    public Map<UUID, PlayerData> getPlayerData() {
+        return playerData;
     }
 }
