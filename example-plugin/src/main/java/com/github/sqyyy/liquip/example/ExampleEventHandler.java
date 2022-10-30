f891882fdca2e6fa3c0e6024875352990eec8a5e
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-30 15:51:56 +0100

    [dev] Add damage-listener for example-plugin "Staff Of Power"

--- example-plugin/src/main/java/com/github/sqyyy/liquip/example/ExampleEventHandler.java
+++ example-plugin/src/main/java/com/github/sqyyy/liquip/example/ExampleEventHandler.java
@@ -2,6 +2,7 @@ package com.github.sqyyy.liquip.example;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -20,5 +21,12 @@ public class ExampleEventHandler implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         plugin.getPlayerData().remove(event.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onLightingStrike(EntityDamageByEntityEvent event) {
+        if (event.getDamager().getScoreboardTags().contains("liquip:staff_of_power_lightning")) {
+            event.setDamage(event.getDamage() * 6);
+        }
     }
 }
