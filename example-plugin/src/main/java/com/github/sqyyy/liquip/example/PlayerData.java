8bfe8384a47f411b0b431340c4b3fdb91e2cf778
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-10-30 15:50:09 +0100

    [dev] Remove SonicShot from PlayerData

--- example-plugin/src/main/java/com/github/sqyyy/liquip/example/PlayerData.java
+++ example-plugin/src/main/java/com/github/sqyyy/liquip/example/PlayerData.java
@@ -1,13 +1,10 @@
 package com.github.sqyyy.liquip.example;
 
-import com.github.sqyyy.liquip.example.features.StaffOfPower;
-
 public class PlayerData {
     // AspectOfTheEnd
     private long aspectOfTheEndCooldown = 0;
     // StaffOfPower
     private long staffOfPowerCooldown = 0;
-    private StaffOfPower.SonicShot staffOfPowerSonicShot = null;
 
     // AspectOfTheEnd
     public long getAspectOfTheEndCooldown() {
@@ -41,13 +38,5 @@ public class PlayerData {
 
     public void cooldownStaffOfPower(long millis) {
         staffOfPowerCooldown = System.currentTimeMillis() + millis;
-    }
-
-    public StaffOfPower.SonicShot getStaffOfPowerSonicShot() {
-        return staffOfPowerSonicShot;
-    }
-
-    public void setStaffOfPowerSonicShot(StaffOfPower.SonicShot staffOfPowerSonicShot) {
-        this.staffOfPowerSonicShot = staffOfPowerSonicShot;
     }
 }
