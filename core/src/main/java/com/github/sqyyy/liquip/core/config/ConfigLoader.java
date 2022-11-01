271753e14b764e6a3460759791b1df8f69a69e60
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-01 11:22:21 +0100

    Improve logging * Migrate logging system to ComponentLogger and MiniMessage * Improve readability of logs

--- core/src/main/java/com/github/sqyyy/liquip/core/config/ConfigLoader.java
+++ core/src/main/java/com/github/sqyyy/liquip/core/config/ConfigLoader.java
@@ -5,6 +5,8 @@ import com.github.sqyyy.liquip.core.items.LiquipItem;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -16,13 +18,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class ConfigLoader {
-    private Logger logger = null;
+    private ComponentLogger componentLogger = null;
     private Config currentItemConfig = null;
     private List<String> itemPaths = null;
 
     public boolean loadConfig() {
         Plugin plugin = Liquip.getProvidingPlugin(Liquip.class);
-        logger = plugin.getComponentLogger();
+        componentLogger = plugin.getComponentLogger();
         final Path pluginPath = Path.of(plugin.getDataFolder().toURI());
         if (!createDirectory(pluginPath, false)) {
             return false;
@@ -40,7 +42,7 @@ public class ConfigLoader {
         }
         for (String itemPathString : itemPaths) {
             if (itemPathString.contains("..")) {
-                log("Path to file for item '{}' cannot include '..'", true, itemPathString);
+                log("Path to file for item <aqua>'{}'</aqua> cannot include '..'", true, itemPathString);
                 continue;
             }
             final Path itemPath = itemsPath.resolve(itemPathString);
@@ -57,7 +59,7 @@ public class ConfigLoader {
             return;
         }
         final Optional<LiquipItem> itemResult =
-                LiquipItem.fromConfig(logger, path, currentItemConfig, Liquip.getProvider());
+                LiquipItem.fromConfig(componentLogger, path, currentItemConfig, Liquip.getProvider());
         if (itemResult.isEmpty()) {
             return;
         }
@@ -65,7 +67,7 @@ public class ConfigLoader {
         if (Liquip.getProvider().getItemRegistry().register(item.getId(), item)) {
             return;
         }
-        log("Could not register item '{}'", true, item.getId());
+        log("Could not register item <aqua>'{}'</aqua>", true, item.getId());
     }
 
     private boolean createDirectory(Path path, boolean warnOnly) {
@@ -73,13 +75,13 @@ public class ConfigLoader {
             if (Files.isDirectory(path)) {
                 return true;
             }
-            log("Could not create directory '{}' as it's a file", warnOnly, path.toAbsolutePath());
+            log("Could not create directory <aqua>'{}'</aqua> as it's a file", warnOnly, path.toAbsolutePath());
             return false;
         }
         try {
             Files.createDirectory(path);
         } catch (IOException e) {
-            log("Could not create directory '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not create directory <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         return true;
@@ -90,14 +92,14 @@ public class ConfigLoader {
             if (!Files.isDirectory(path)) {
                 return true;
             }
-            log("Could not create file '{}' as it's a directory", warnOnly, path.toAbsolutePath());
+            log("Could not create file <aqua>'{}'</aqua> as it's a directory", warnOnly, path.toAbsolutePath());
             return false;
         }
         try {
             Files.createFile(path);
             Files.writeString(path, contents);
         } catch (IOException e) {
-            log("Could not create or write to file '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not create or write to file <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         return true;
@@ -108,11 +110,11 @@ public class ConfigLoader {
         try {
             config = ConfigFactory.parseFile(path.toFile());
         } catch (ConfigException exception) {
-            log("Could not parse file '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         if (config == null) {
-            log("Could not parse file '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         if (!config.hasPath("items")) {
@@ -143,34 +145,34 @@ public class ConfigLoader {
         try {
             currentItemConfig = ConfigFactory.parseFile(path.toFile());
         } catch (ConfigException exception) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         if (currentItemConfig == null) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             return false;
         }
         if (!currentItemConfig.hasPath("id")) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  Config has no id-tag. For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items",
                     warnOnly);
             return false;
         }
         if (!currentItemConfig.hasPath("material")) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  Config has no material-tag. For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items",
                     warnOnly);
             return false;
         }
         if (!currentItemConfig.hasPath("name")) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  Config has no name-tag", warnOnly);
             log("  For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items", warnOnly);
             return false;
         }
         if (!checkItemConfigString(path, "id", true) || !checkItemConfigString(path, "material", true) ||
                 !checkItemConfigString(path, "name", true)) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  Config has no valid id, material and name tags", warnOnly);
             log("  For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items", warnOnly);
             return false;
@@ -181,18 +183,18 @@ public class ConfigLoader {
     private boolean checkItemConfigString(Path path, String tag, boolean warnOnly) {
         try {
             if (currentItemConfig.getString(tag) == null) {
-                log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+                log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
                 log("  The tag '{}' is null", warnOnly, tag);
                 log("  For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items", warnOnly);
                 return false;
             }
         } catch (ConfigException.WrongType exception) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  The tag '{}' has a wrong type", warnOnly, tag);
             log("  For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items", warnOnly);
             return false;
         } catch (ConfigException exception) {
-            log("Could not parse file for item '{}'", warnOnly, path.toAbsolutePath());
+            log("Could not parse file for item <aqua>'{}'</aqua>", warnOnly, path.toAbsolutePath());
             log("  The tag '{}' is invalid", warnOnly, tag);
             log("  For more information read https://github.com/sqyyy-jar/liquip/wiki/Custom-items", warnOnly);
             return false;
@@ -205,18 +207,18 @@ public class ConfigLoader {
             if (!Files.isDirectory(path)) {
                 return true;
             }
-            log("Path for item '{}' should not be a directory", warnOnly, path.toAbsolutePath());
+            log("Path for item <aqua>'{}'</aqua> should not be a directory", warnOnly, path.toAbsolutePath());
             return false;
         }
-        log("File for item '{}' does not exist", warnOnly, path.toAbsolutePath());
+        log("File for item <aqua>'{}'</aqua> does not exist", warnOnly, path.toAbsolutePath());
         return false;
     }
 
     private void log(String message, boolean warnOnly, Object... args) {
         if (warnOnly) {
-            logger.warn(message, args);
+            componentLogger.warn(MiniMessage.miniMessage().deserialize(message), args);
             return;
         }
-        logger.error(message, args);
+        componentLogger.error(MiniMessage.miniMessage().deserialize(message), args);
     }
 }
