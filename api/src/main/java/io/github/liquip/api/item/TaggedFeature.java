01e2dd9b3ed702f6659ae5577e40391f2bfe9776
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-03 18:33:55 +0100

    Migrate TaggedFeature to new config api

--- api/src/main/java/io/github/liquip/api/item/TaggedFeature.java
+++ api/src/main/java/io/github/liquip/api/item/TaggedFeature.java
@@ -1,13 +1,13 @@
 package io.github.liquip.api.item;
 
-import io.github.liquip.api.config.ConfigTable;
+import io.github.liquip.api.config.ConfigElement;
 import org.bukkit.Keyed;
 import org.bukkit.inventory.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public interface TaggedFeature<T> extends Keyed {
-    default @Nullable T initialize(@NonNull Item item, @NonNull ConfigTable configTable) {
+    default @Nullable T initialize(@NonNull Item item, @NonNull ConfigElement configTable) {
         return null;
     }
 
