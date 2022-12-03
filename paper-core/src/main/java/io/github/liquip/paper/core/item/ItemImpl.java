0052a34f45f3cbb40f3f6cc0d9e04ee663745031
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-03 18:34:17 +0100

    Add enchantments to ItemImpl

--- paper-core/src/main/java/io/github/liquip/paper/core/item/ItemImpl.java
+++ paper-core/src/main/java/io/github/liquip/paper/core/item/ItemImpl.java
@@ -1,13 +1,16 @@
 package io.github.liquip.paper.core.item;
 
 import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import io.github.liquip.api.Liquip;
-import io.github.liquip.api.LiquipProvider;
+import io.github.liquip.api.config.ConfigElement;
+import io.github.liquip.api.item.Enchantment;
 import io.github.liquip.api.item.Feature;
 import io.github.liquip.api.item.Item;
 import io.github.liquip.api.item.TaggedFeature;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
@@ -19,54 +22,42 @@ import org.jetbrains.annotations.NotNull;
 import java.util.*;
 import java.util.function.BiConsumer;
 
+@SuppressWarnings("unchecked")
 public class ItemImpl implements Item {
-    private final Liquip API;
+    private final Liquip api;
     private final Key key;
     private final Material material;
     private final Component displayName;
     private final List<Component> lore;
+    private final Object2IntMap<Enchantment> enchantments;
     private final List<Feature> features;
     private final Map<TaggedFeature<?>, Object> taggedFeatures;
     private final Multimap<Class<? extends Event>, BiConsumer<? extends Event, ItemStack>>
         eventHandlers;
 
-    public ItemImpl(@NonNull Key key, @NonNull Material material, @NonNull Component displayName,
-        @NonNull List<Component> lore) {
-        this(key, material, displayName, lore, List.of(), Map.of(), ImmutableMultimap.of());
-    }
-
     public ItemImpl(@NonNull Liquip api, @NonNull Key key, @NonNull Material material,
-        @NonNull Component displayName, @NonNull List<Component> lore) {
-        this(api, key, material, displayName, lore, List.of(), Map.of(), ImmutableMultimap.of());
-    }
-
-    public ItemImpl(@NonNull Key key, @NonNull Material material, @NonNull Component displayName,
-        @NonNull List<Component> lore, @NonNull List<Feature> features,
-        @NonNull Map<TaggedFeature<?>, Object> taggedFeatures,
-        @NonNull Multimap<Class<? extends Event>, BiConsumer<? extends Event, ItemStack>> eventHandlers) {
-        this(LiquipProvider.get(), key, material, displayName, lore, features, taggedFeatures,
-            eventHandlers);
-    }
-
-    public ItemImpl(@NonNull Liquip api, @NonNull Key key, @NonNull Material material,
-        @NonNull Component displayName, @NonNull List<Component> lore,
-        @NonNull List<Feature> features, @NonNull Map<TaggedFeature<?>, Object> taggedFeatures,
-        @NonNull Multimap<Class<? extends Event>, BiConsumer<? extends Event, ItemStack>> eventHandlers) {
-        this.API = api;
+                    @NonNull Component displayName, @NonNull List<Component> lore,
+                    @NonNull Object2IntMap<Enchantment> enchantments,
+                    @NonNull List<Feature> features,
+                    @NonNull Map<TaggedFeature<?>, ConfigElement> taggedFeatures,
+                    @NonNull Multimap<Class<? extends Event>, BiConsumer<? extends Event, ItemStack>> eventHandlers) {
+        this.api = api;
         this.key = key;
         this.material = material;
         this.displayName = displayName;
-        this.lore = new ArrayList<>();
+        this.lore = new ArrayList<>(lore.size());
         this.lore.addAll(lore);
-        this.features = new ArrayList<>();
+        this.enchantments = new Object2IntOpenHashMap<>(enchantments.size());
+        this.enchantments.putAll(enchantments);
+        this.features = new ArrayList<>(features.size());
         this.features.addAll(features);
-        this.taggedFeatures = new LinkedHashMap<>();
-        this.taggedFeatures.putAll(taggedFeatures);
+        this.taggedFeatures = new LinkedHashMap<>(taggedFeatures.size());
+        this.taggedFeatures.putAll(Maps.transformEntries(taggedFeatures,
+            (taggedFeature, config) -> taggedFeature.initialize(this, config)));
         this.eventHandlers = ArrayListMultimap.create();
         this.eventHandlers.putAll(eventHandlers);
     }
 
-    @SuppressWarnings("unchecked")
     private <T> void applyToTaggedFeature(TaggedFeature<T> key, ItemStack itemStack, Object value) {
         key.apply(this, itemStack, (T) value);
     }
@@ -82,17 +73,17 @@ public class ItemImpl implements Item {
         itemStack.editMeta(meta -> {
             meta.displayName(this.displayName);
             meta.lore(this.lore);
-            API.setKeyForItemStack(itemStack, this.key);
+            api.setKeyForItemStack(itemStack, this.key);
         });
+        enchantments.forEach((enchantment, level) -> enchantment.apply(this, itemStack, level));
         features.forEach(feature -> feature.apply(this, itemStack));
         taggedFeatures.forEach((key, value) -> this.applyToTaggedFeature(key, itemStack, value));
         return itemStack;
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public <T extends Event> void callEvent(@NonNull Class<T> eventClass, @NonNull T event,
-        @NonNull ItemStack itemStack) {
+                                            @NonNull ItemStack itemStack) {
         final Collection<BiConsumer<? extends Event, ItemStack>> handlers =
             eventHandlers.get(eventClass);
         for (BiConsumer<? extends Event, ItemStack> handler : handlers) {
@@ -102,7 +93,7 @@ public class ItemImpl implements Item {
 
     @Override
     public <T extends Event> void registerEvent(@NonNull Class<T> eventClass,
-        @NonNull BiConsumer<T, ItemStack> eventHandler) {
+                                                @NonNull BiConsumer<T, ItemStack> eventHandler) {
         eventHandlers.put(eventClass, eventHandler);
     }
 }
