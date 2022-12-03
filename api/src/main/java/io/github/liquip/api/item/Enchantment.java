7a82be22daae4d78d0cc287d5b3f3df4129f05fc
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-03 18:33:29 +0100

    Add new abilities to Enchantment

--- api/src/main/java/io/github/liquip/api/item/Enchantment.java
+++ api/src/main/java/io/github/liquip/api/item/Enchantment.java
@@ -1,6 +1,13 @@
 package io.github.liquip.api.item;
 
 import org.bukkit.Keyed;
+import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public interface Enchantment extends Keyed {
+    default void initialize(@NonNull Item item, int level) {
+    }
+
+    default void apply(@NonNull Item item, @NonNull ItemStack itemStack, int level) {
+    }
 }
