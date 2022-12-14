58d2df480e9b339b56856e641f678a37737abaea
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-14 20:31:06 +0100

    Add crafting methods to Recipe

--- api/src/main/java/io/github/liquip/api/item/crafting/Recipe.java
+++ api/src/main/java/io/github/liquip/api/item/crafting/Recipe.java
@@ -1,10 +1,17 @@
 package io.github.liquip.api.item.crafting;
 
+import net.kyori.adventure.key.KeyedValue;
 import org.bukkit.inventory.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
 
 public interface Recipe {
     @NonNull CraftMatrix getMatrix();
 
     boolean matches(@NonNull ItemStack @NonNull [] craftingMatrix);
+
+    void apply(@NonNull ItemStack @NonNull [] stacks);
+
+    @NonNull ItemStack getResult(List<KeyedValue<Integer>> stacks);
 }
