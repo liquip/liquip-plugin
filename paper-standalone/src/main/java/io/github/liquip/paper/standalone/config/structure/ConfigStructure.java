76a81d4049b30884dc662c44a5506be57c978482
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-16 20:24:21 +0100

    Add crafting table item to ConfigStructure

--- paper-standalone/src/main/java/io/github/liquip/paper/standalone/config/structure/ConfigStructure.java
+++ paper-standalone/src/main/java/io/github/liquip/paper/standalone/config/structure/ConfigStructure.java
@@ -2,11 +2,14 @@ package io.github.liquip.paper.standalone.config.structure;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.util.List;
 
 public class ConfigStructure {
     private final List<String> items;
+    private boolean customCraftingTable = false;
+    private String craftingTable = "liquip:crafting_table";
 
     @JsonCreator
     public ConfigStructure(@JsonProperty(value = "items", required = true) List<String> items) {
@@ -15,5 +18,19 @@ public class ConfigStructure {
 
     public List<String> getItems() {
         return this.items;
+    }
+
+    public boolean hasCustomCraftingTable() {
+        return this.customCraftingTable;
+    }
+
+    public String getCraftingTable() {
+        return this.craftingTable;
+    }
+
+    @JsonSetter
+    public void setCraftingTable(String craftingTable) {
+        this.customCraftingTable = true;
+        this.craftingTable = craftingTable;
     }
 }
