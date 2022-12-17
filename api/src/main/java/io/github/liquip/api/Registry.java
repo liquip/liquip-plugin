ca472da8546247e4aeab5d77e58edeb382b43031
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-17 21:40:44 +0100

    Improve Registry documentation

--- api/src/main/java/io/github/liquip/api/Registry.java
+++ api/src/main/java/io/github/liquip/api/Registry.java
@@ -6,7 +6,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
- * A registry holding values accessible by a {@link Key}.
+ * A registry holding values of type {@link T} accessible by a {@link Key}.
  *
  * @param <T> the type of value being stored
  * @since 0.0.1-alpha
@@ -15,8 +15,8 @@ public interface Registry<T extends Keyed> extends Iterable<T> {
     /**
      * Registers the specified {@link T} under the specified {@link Key}.
      *
-     * @param key   the {@link Key} the {@link T} will be stored under
-     * @param value the {@link T} that will be stored
+     * @param key   the key the value will be stored under
+     * @param value the value that will be stored
      * @since 0.0.1-alpha
      */
     void register(@NonNull Key key, @NonNull T value);
@@ -24,8 +24,8 @@ public interface Registry<T extends Keyed> extends Iterable<T> {
     /**
      * Retrieves the {@link T} stored under the specified {@link Key}.
      *
-     * @param key the {@link Key} the {@link T} is stored under
-     * @return the {@link T} that is stored under the {@link Key}
+     * @param key the key the value is stored under
+     * @return the value that is stored under the specified key
      * @since 0.0.1-alpha
      */
     @Nullable T get(@NonNull Key key);
