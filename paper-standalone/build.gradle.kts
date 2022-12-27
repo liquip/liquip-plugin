a28c3c0090d78413e925a3aea181e1d5484204c5
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-27 20:17:02 +0100

    Fix build file

--- paper-standalone/build.gradle.kts
+++ paper-standalone/build.gradle.kts
@@ -15,10 +15,10 @@ repositories {
         credentials {
             username = project.findProperty("gpr.user") as String? ?: if (System.getenv()
                     .containsKey("GRADLE_GITHUB_USERNAME")
-            ) System.getenv("GRADLE_GITHUB_USERNAME") else System.getenv("USERNAME")
+            ) System.getenv("GRADLE_GITHUB_USERNAME") else System.getenv("GITHUB_USERNAME")
             password = project.findProperty("gpr.key") as String? ?: if (System.getenv()
                     .containsKey("GRADLE_GITHUB_TOKEN")
-            ) System.getenv("GRADLE_GITHUB_TOKEN") else System.getenv("TOKEN")
+            ) System.getenv("GRADLE_GITHUB_TOKEN") else System.getenv("GITHUB_TOKEN")
         }
     }
 }
