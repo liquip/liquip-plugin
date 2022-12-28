b25ec2008d74db7ab50d4dff136a6d894c2ad104
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-28 21:51:00 +0100

    Add iterator methods to CraftingSystem

--- api/src/main/java/io/github/liquip/api/item/crafting/CraftingSystem.java
+++ api/src/main/java/io/github/liquip/api/item/crafting/CraftingSystem.java
@@ -2,6 +2,9 @@ package io.github.liquip.api.item.crafting;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
 
 public interface CraftingSystem {
     void registerShapedRecipe(@NonNull ShapedRecipe recipe);
@@ -11,4 +14,8 @@ public interface CraftingSystem {
     @Nullable ShapedRecipe getShapedRecipe(@NonNull CraftMatrix craftMatrix);
 
     @Nullable ShapelessRecipe getShapelessRecipe(@NonNull CraftMatrix craftMatrix);
+
+    @NotNull Iterator<ShapedRecipe> shapedIterator();
+
+    @NotNull Iterator<ShapelessRecipe> shapelessIterator();
 }
