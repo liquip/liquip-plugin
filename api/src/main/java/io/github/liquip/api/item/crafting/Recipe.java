0f1b7718c915f34ad8814782f43b24bb4178a1bd
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-28 21:58:55 +0100

    Update Recipe API

--- api/src/main/java/io/github/liquip/api/item/crafting/Recipe.java
+++ api/src/main/java/io/github/liquip/api/item/crafting/Recipe.java
@@ -3,6 +3,7 @@ package io.github.liquip.api.item.crafting;
 import net.kyori.adventure.key.KeyedValue;
 import org.bukkit.inventory.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -12,4 +13,6 @@ public interface Recipe {
     void apply(@NonNull ItemStack @NonNull [] stacks);
 
     @NonNull ItemStack getResult(@NonNull List<KeyedValue<Integer>> stacks);
+
+    @NotNull ItemStack getShowcaseItem();
 }
