20cecbde5cabf8979fba18ef48a43262c7db25f1
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2024-04-28 17:58:01 +0200

    chore: remove redundant comment

--- paper-standalone/src/main/java/io/github/liquip/paper/standalone/config/structure/ConfigStructure.java
+++ paper-standalone/src/main/java/io/github/liquip/paper/standalone/config/structure/ConfigStructure.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-/**
- * @param craftingTable = "liquip:crafting_table" TODO remove
- */
 public record ConfigStructure(@JsonProperty(value = "items", required = true) List<String> items,
                               @JsonProperty("craftingTable") String craftingTable) {
 }
